a11c7a678ef4b39e32a9f97f6aa66e3ad7b774c9
alphaqcode <duset@126.com>
2015-07-15 19:52:34 +0800

    make inspectable

--- Happyness/Happyness/FaceView.swift
+++ Happyness/Happyness/FaceView.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
+@IBDesignable
+
 class FaceView: UIView {
-    
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay()}}
+    
+    @IBInspectable
     var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay()}}
+    @IBInspectable
     var scale: CGFloat = 0.90 { didSet { setNeedsDisplay()}}
     var faceCenter: CGPoint {
         return convertPoint(center, fromView: superview)
